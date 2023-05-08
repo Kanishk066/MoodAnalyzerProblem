8ad9ea0dee36e8ec9f94f383dfe3ae9fec385ba5
Kanishk <kanishksinghal06@gmail.com>
2023-05-08 17:04:37 +0530

    Repeated the test case 1 again

--- src/main/java/com/bl/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bl/moodanalyzer/MoodAnalyzer.java
@@ -21,6 +21,11 @@ public class MoodAnalyzer {
         MoodAnalyzer analyser = new MoodAnalyzer("I am feeling happy");
         String mood = analyser.analyseMood();
         System.out.println("The mood is " + mood);
-
+        MoodAnalyzer.testAnalyseMood();
+    }
+    public static void testAnalyseMood(){
+        MoodAnalyzer analyser = new MoodAnalyzer("I am in Sad Mood");
+       String mood = analyser.analyseMood();
+        assert mood.equals("Sad") : "Test Case 1.1 (with constructor) Failed";
     }
 }
