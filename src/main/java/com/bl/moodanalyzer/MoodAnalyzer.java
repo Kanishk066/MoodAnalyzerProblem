7fad24a4bc24446e46f963af71ba636288b9fadb
Kanishk <kanishksinghal06@gmail.com>
2023-05-08 16:08:35 +0530

    Added the code for analysing and respond mood

--- src/main/java/com/bl/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bl/moodanalyzer/MoodAnalyzer.java
@@ -1,7 +1,25 @@
 package com.bl.moodanalyzer;
 
 public class MoodAnalyzer {
+     String message;
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        if (message.contains("happy")) {
+            return "Happy";
+        } else if (message.contains("sad")) {
+            return "Sad";
+        } else {
+            return "Unknown";
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the Mood Analyzer Problem");
+        MoodAnalyzer analyser = new MoodAnalyzer("I am feeling sad today");
+        String mood = analyser.analyseMood();
+        System.out.println("Mood: " + mood);
     }
 }
