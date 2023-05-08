6fdadc6a6a6b5c691de690755460ed648f09396f
Kanishk <kanishksinghal06@gmail.com>
2023-05-08 22:00:45 +0530

    Added the test case for custom exception

--- src/main/java/com/bl/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bl/moodanalyzer/MoodAnalyzer.java
@@ -2,10 +2,8 @@ package com.bl.moodanalyzer;
 enum ExceptionType {
     NULL_MESSAGE, EMPTY_MESSAGE, NO_SUCH_FIELD, NO_SUCH_METHOD, NO_SUCH_CLASS, OBJECT_CREATION_ISSUE
 }
-
 class MoodAnalysisException extends Exception {
-    private ExceptionType type;
-
+     ExceptionType type;
     public MoodAnalysisException(ExceptionType type, String message) {
         super(message);
         this.type = type;
@@ -16,14 +14,12 @@ public class MoodAnalyzer {
     public MoodAnalyzer() throws MoodAnalysisException {
         throw new MoodAnalysisException(ExceptionType.EMPTY_MESSAGE, "Message should not be empty");
     }
-
     public MoodAnalyzer(String message) throws MoodAnalysisException {
         if (message == null || message.isEmpty()) {
             throw new MoodAnalysisException(ExceptionType.NULL_MESSAGE, "Message should not be null");
         }
         this.message = message;
     }
-
     public String analyseMood() throws MoodAnalysisException {
         try {
             if (message.contains("sad")) {
@@ -36,5 +32,21 @@ public class MoodAnalyzer {
         }
     }
     public static void main(String[] args) {
+        MoodAnalyzer.testAnalyseMood();
+    }
+    public static void testAnalyseMood(){
+        MoodAnalyzer analyser;
+        try {
+            analyser = new MoodAnalyzer(null);
+            analyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            assert e.type.equals(ExceptionType.NULL_MESSAGE) : "Test Case 3.1 Failed";
+        }
+        try {
+            analyser = new MoodAnalyzer("");
+        } catch (MoodAnalysisException e) {
+            assert e.type.equals(ExceptionType.EMPTY_MESSAGE) : "Test Case 3.2 Failed";
+        }
     }
 }
+
